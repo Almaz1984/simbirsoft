848069a3ff36b755468792c8724da919aca26881
Almaz Makhmutov <almaz1984@gmail.com>
2022-02-02 23:11:02 +0500

    change Application title change Application icon add Splash screen activity update gradle dependencies

---
+++ app/src/main/java/com/almaz/task1/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.almaz.task1
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        routeToAppropriatePage()
+    }
+
+    private fun routeToAppropriatePage() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
